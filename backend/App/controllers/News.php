8498b5e710965c90f57b9e432f406608fa19b004
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-02-11 11:21:04 -0600

    Pagina de noticias

--- backend/App/controllers/News.php
+++ backend/App/controllers/News.php
@@ -24,6 +24,33 @@ class News extends Controller{
 
     public function index() {
      $extraHeader =<<<html
+
+     <!-- Basic Page Needs
+     ================================================== -->
+     <meta charset="utf-8">
+     <title>Small Apps | Bootstrap App Landing Template</title>
+   
+     <!-- Mobile Specific Metas
+     ================================================== -->
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="description" content="Bootstrap App Landing Template">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+     <meta name="author" content="Themefisher">
+     <meta name="generator" content="Themefisher Small Apps Template v1.0">
+   
+     <!-- Favicon -->
+     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+     
+     <!-- PLUGINS CSS STYLE -->
+     <link rel="stylesheet" href="../../assets/assets_news/plugins/bootstrap/bootstrap.min.css">
+     <link rel="stylesheet" href="../../assets/assets_news/plugins/themify-icons/themify-icons.css">
+     <link rel="stylesheet" href="../../assets/assets_news/plugins/slick/slick.css">
+     <link rel="stylesheet" href="../../assets/assets_news/plugins/slick/slick-theme.css">
+     <link rel="stylesheet" href="../../assets/assets_news/plugins/fancybox/jquery.fancybox.min.css">
+     <link rel="stylesheet" href="../../assets/assets_news/plugins/aos/aos.css">
+   
+     <!-- CUSTOM CSS -->
+     <link href="css/style.css" rel="stylesheet">
 html;
      $extraFooter =<<<html
 
