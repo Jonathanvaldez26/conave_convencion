914516c7236d68a0370dae3f6438fbdab25e9e1c
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-02-15 14:13:09 -0600

    Correccion de ancho de carta - Cenas

--- backend/App/views/dinners_all.php
+++ backend/App/views/dinners_all.php
@@ -55,7 +55,7 @@
     <div class="container-fluid py-4">
         <div class="row">
             <div class="col-1"></div>
-            <div class="col-10">
+            <div class="col-12">
                 <div class="card">
                     <div class="card-header p-3">
                         <h5 class="mb-2">Cenas</h5>
