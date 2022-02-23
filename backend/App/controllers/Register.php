563fff6270409e208a2800a6bb1f57d75246e943
Gerson Velasco <gvelassco@gmail.com>
2022-02-23 09:14:07 -0600

    prueba register1

--- backend/App/controllers/Register.php
+++ backend/App/controllers/Register.php
@@ -15,7 +15,150 @@ class Register{
     private $_contenedor;
 
     public function index() {
-        echo "prueba de controlador";
+        $extraHeader =<<<html
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/favicon.ico">
+        <link rel="icon" type="image/vnd.microsoft.icon" href="/assets//img/favicon.ico">
+        <title>
+            APM Register
+        </title>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+        <!-- Nucleo Icons -->
+        <link href="../../../assets/css/nucleo-icons.css" rel="stylesheet" />
+        <link href="../../../assets/css/nucleo-svg.css" rel="stylesheet" />
+        <!-- Font Awesome Icons -->
+        <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+        <link href="../../../assets/css/nucleo-svg.css" rel="stylesheet" />
+        <!-- CSS Files -->
+        <link id="pagestyle" href="../../../assets/css/soft-ui-dashboard.css?v=1.0.5" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+        <!-- Nucleo Icons -->
+        <link href="../../../assets/css/nucleo-icons.css" rel="stylesheet" />
+        <link href="../../../assets/css/nucleo-svg.css" rel="stylesheet" />
+        <!-- Font Awesome Icons -->
+        <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+        <link href="../../../assets/css/nucleo-svg.css" rel="stylesheet" />
+        <!-- CSS Files -->
+        <link id="pagestyle" href="/assets/css/soft-ui-dashboard.css?v=1.0.5" rel="stylesheet" />
+        <link rel="stylesheet" href="/css/alertify/alertify.core.css" />
+        <link rel="stylesheet" href="/css/alertify/alertify.default.css" id="toggleCSS" />
+        <!--script src="../../../assets/js/plugins/choices.min.js"></script-->
+
+        
+        
+        
+
+html;
+        $extraFooter =<<<html
+     
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/validate/jquery.validate.js"></script>
+        <script src="/js/alertify/alertify.min.js"></script>
+        <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+       <!--   Core JS Files   -->
+          <script src="../../../assets/js/core/popper.min.js"></script>
+          <script src="../../../assets/js/core/bootstrap.min.js"></script>
+          <script src="../../../assets/js/plugins/perfect-scrollbar.min.js"></script>
+          <script src="../../../assets/js/plugins/smooth-scrollbar.min.js"></script>
+          <script src="../../../assets/js/plugins/multistep-form.js"></script>
+          <script src="../../../assets/js/plugins/choices.min.js"></script>
+          <script type="text/javascript" wfd-invisible="true">
+            if (document.getElementById('choices-button')) {
+                var element = document.getElementById('choices-button');
+                const example = new Choices(element, {});
+            }
+            var choicesTags = document.getElementById('choices-tags');
+            var color = choicesTags.dataset.color;
+            if (choicesTags) {
+                const example = new Choices(choicesTags, {
+                delimiter: ',',
+                editItems: true,
+                maxItemCount: 5,
+                removeItemButton: true,
+                addItems: true,
+                classNames: {
+                    item: 'badge rounded-pill choices-' + color + ' me-2'
+                }
+                });
+            }
+        </script>
+          <!-- Kanban scripts -->
+          <script src="../../../assets/js/plugins/dragula/dragula.min.js"></script>
+          <script src="../../../assets/js/plugins/jkanban/jkanban.js"></script>
+          <script>
+            var win = navigator.platform.indexOf('Win') > -1;
+            if (win && document.querySelector('#sidenav-scrollbar')) {
+              var options = {
+                damping: '0.5'
+              }
+              Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+            }
+          </script>
+          <!-- Github buttons -->
+          <script async defer src="https://buttons.github.io/buttons.js"></script>
+          <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+           
+
+          <script>
+            $(document).ready(function(){
+                $.validator.addMethod("checkUserCorreo",function(value, element) {
+                  var response = false;
+                    $.ajax({
+                        type:"POST",
+                        async: false,
+                        url: "/Register/isUserValidate",
+                        data: {email: $("#email").val()},
+                        success: function(data) {
+                            if(data=="true"){
+                                $('#btn_registro_email').attr("disabled", false);
+                                $('#confirm_email').attr("disabled", false);
+                                $('#email').attr("disabled", true);
+
+                                response = true;
+                            }else{
+                                $('#btn_registro_email').attr("disabled", true);
+                                $('#confirm_email').attr("disabled", true);
+                                document.getElementById("confirm_email").value = "";
+                            }
+                        }
+                    });
+
+                    return response;
+                },"Usted no está registrado en la Base de Datos CONAVE 2022, verifique con su área y reintente.");
+
+                $("#email_form").validate({
+                   rules:{
+                        email:{
+                            required: true,
+                            checkUserCorreo: true
+                        },
+                        confirm_email:{
+                            required: true,
+                            equalTo:"#email"
+                        }
+                    },
+                    messages:{
+                        email:{
+                            required: "Este campo es requerido",
+                        },
+                        confirm_email:{
+                            required: "Este campo es requerido",
+                            equalTo: "El Correo Eléctronico no coincide",
+                        }
+                    }
+                });
+
+                
+
+
+            });
+        </script>
+       
+html;
+        View::set('header',$extraHeader);
+        View::set('footer',$extraFooter);
+        View::render("register");
     }
 
 }
