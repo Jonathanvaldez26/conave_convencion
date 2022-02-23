9a64bb87d5803e513268b2a0b3093cafcca33198
Gerson Velasco <gvelassco@gmail.com>
2022-02-23 08:44:44 -0600

    solucion de login

--- backend/App/controllers/Login.php
+++ backend/App/controllers/Login.php
@@ -120,6 +120,7 @@ html;
                         url: "/Login/verificarUsuario",
                         data: $("#login").serialize(),
                         success: function(response){
+                            //console.log(response);
                             if(response!=""){
                                 var usuario = jQuery.parseJSON(response);
                                 if(usuario.nombre!=""){
--- backend/App/models/Login.php
+++ backend/App/models/Login.php
@@ -10,7 +10,7 @@ class Login{
     public static function getById($usuario){
         $mysqli = Database::getInstance(true);
         $query =<<<sql
-         SELECT * FROM utilerias_administradores WHERE usuario LIKE :usuario
+        SELECT ua.*, ra.nombre FROM utilerias_administradores ua INNER JOIN registros_acceso ra WHERE ua.id_registro = ra.id_registro and ua.usuario LIKE :usuario
 sql;
         $params = array(
             ':usuario'=> $usuario->_usuario
