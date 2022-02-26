72fd18c9a90d5169391ef47864dfadc0c5d3577a
Your Name <jonathan.valdez@grupolahe.com>
2022-02-26 14:26:28 -0600

    Ef

--- backend/App/models/Home.php
+++ backend/App/models/Home.php
@@ -12,6 +12,7 @@ class Home{
     public static function getCountByUser($id){
       $mysqli = Database::getInstance();
       $query=<<<sql
+    SELECT count(*) as count from pickup where utilerias_asistentes_id = '$id';
 sql;
       return $mysqli->queryAll($query);
     }
