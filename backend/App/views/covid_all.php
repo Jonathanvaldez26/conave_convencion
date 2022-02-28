0cc6674246fb2c398d95ca427eaac06111c51bbe
Your Name <jonathan.valdez@grupolahe.com>
2022-02-28 16:35:57 -0600

    fd

--- backend/App/views/covid_all.php
+++ backend/App/views/covid_all.php
@@ -91,6 +91,11 @@
                                 <br>
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="button-row d-flex mt-4 col-12">
+                                <a class="btn bg-gradient-light mb-0 js-btn-prev" href="/Home/" title="Prev">Regresar</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
