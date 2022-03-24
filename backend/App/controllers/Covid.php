7c9dcfcac5a6e63200f96332f96a39f101bbfc4f
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-03-24 12:13:09 -0600

    Deshabilitar boton aceptar en Pruebas Covid

--- backend/App/controllers/Covid.php
+++ backend/App/controllers/Covid.php
@@ -85,6 +85,8 @@ html;
           $("#form_prueba_covid").on("submit",function(event){
               event.preventDefault();
               
+                document.getElementById('btn_upload').setAttribute('disabled','');
+
                   var formData = new FormData(document.getElementById("form_prueba_covid"));
                   console.log(formData);
                   $.ajax({
